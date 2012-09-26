528f79893ebd5c4ca6051e7c9ec257257f7deb42
Björgvin Ragnarsson <nifgraup@gmail.com>
2012-09-26 22:48:26 +0200

    formatted code and run sed in one go, faster

--- makelaw.sh
+++ makelaw.sh
@@ -1,43 +1,50 @@
 #!/bin/bash
 
-#todo:
-#	remove newer timestamping format in html
+# TODO
+#	normalize html
+#		remove newer timestamping format in html
+#		remove "Ferill málsins á Alþingi" (or include it from the beginning)
+#		html tidy
+#		use of <a, <!a and <!!a
 #	fix commit message
 #		timestamp
 #		(Nafn á forseta Íslands)
 #	use github pages for rendering html
-#	normalize html
-#		html tidy
-#		use of <a, <!a and <!!a
+#	tag (& sign) releases
 
+
+# Download zip archives
 wget -N --no-remove-listing http://althingi.is/vefur/eldri-utg.html
 grep -o 'lagas/.*/allt.zip' eldri-utg.html | awk '{ print "http://althingi.is/"$0; }' > urls.txt
-
 wget -m -i urls.txt
 
+
+# Create git repo and configure it
 (
-	git init lagasafn;
-	cd lagasafn;
-	git config user.name "Forseti Íslands";
-	git config user.email "forseti@forseti.is";
+	git init lagasafn
+	cd lagasafn
+	git config user.name "Forseti Íslands"
+	git config user.email "forseti@forseti.is"
 	git remote add origin https://nifgraup@github.com/nifgraup/lagasafn.git
 )
 
 for i in $( ls althingi.is/lagas/ );
 do
 	(
-		cd lagasafn &&
-		git rm -r --ignore-unmatch * &&
-		unzip ../althingi.is/lagas/$i/allt.zip &&
+		cd lagasafn
+		git rm -r --ignore-unmatch *
+		unzip ../althingi.is/lagas/$i/allt.zip
 
-		#filters:
-		#
+		# html normalizing
 		find . -name "*.html" | while read file; do
-			LC_ALL=en_US sed -i '/Prenta.*tveimur/d' $file #remove timestamp
-			sed -i '/<!-- Virk vefmaeling byrjar/,$d' $file #Remove teljar.is & google analytics
+			#remove timestamp
+			#remove teljar.is & google analytics
+			LC_ALL=en_US sed -i -e '/Prenta.*tveimur/d' \
+			                    -e '/<!-- Virk vefmaeling byrjar/,$d' $file
 		done
 
-		git add . && git commit -m "Útgáfa $i";
+		git add .
+		git commit -m "Útgáfa $i"
 	)
 done
 
