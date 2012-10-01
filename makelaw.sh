2d147eb6fe48742a01f65305e92750fa59c34094
Björgvin Ragnarsson <nifgraup@gmail.com>
2012-10-01 14:00:58 +0200

    Convert html to markdown using pandoc. Markdown is rendered by github. There is still an issue with pandoc, it gets stuck in endless loop when processing two of the zip archives

--- makelaw.sh
+++ makelaw.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 # TODO
-#	use markdown?
-#		skip unnecessary html sanitation, let pandoc take care of it.
-#		fix html links to markdown links
+#	markdown
+#		fix infinitive loop in pandoc
+#		fix html links to markdown links. Nb. don't convert external links to althingi.is
 #	normalize html
-#		remove name attribute?
+#		skip unnecessary html sanitation, let pandoc take care of it.
 #		remove newer timestamping format in html
-#		html tidy
 #	fix commit message
-#		timestamp
 #		(Nafn á forseta Íslands)
-#	use github pages for rendering html
 #	tag (& sign) releases
 
 
 # Download zip archives
-wget -N --no-remove-listing http://althingi.is/vefur/eldri-utg.html
+wget -q -N --no-remove-listing http://althingi.is/vefur/eldri-utg.html
 grep -o 'lagas/.*/allt.zip' eldri-utg.html | awk '{ print "http://althingi.is/"$0; }' > urls.txt
-wget -m -i urls.txt
+wget -q -m -i urls.txt
 
 
 # Create git repo and configure it
@@ -33,6 +30,7 @@ wget -m -i urls.txt
 for i in $( ls althingi.is/lagas/ );
 do
 	(
+		echo "Creating commit for version $i"
 		cd lagasafn
 		git rm -q -r --ignore-unmatch *
 		unzip -q ../althingi.is/lagas/$i/allt.zip
@@ -41,14 +39,12 @@ do
 		
 		# html normalizing
 		find . -name "*.html" | while read file; do
-			chmod 644 $file
-	
 			# remove carriage return, required for removal of lines at eof
 			# add newline at end of file, will be removed later if not needed
 			sed -i -e 's/\r//g' \
 				-e '$G' \
 				$file
-
+			
 			# remove timestamp
 			# remove links to althingi.is with info about the process
 			# remove extra newlines at eof
@@ -57,7 +53,7 @@ do
 			# remove absolut link to althingi.is and and replace with a relative link in this dir
 			# remove html & body tags when present at the end of the last line
 			# add head tag if missing
-			LC_ALL=en_US sed -i -e '/Prenta.*tveimur/d' \
+			iconv -f iso-8859-1 $file | sed -e '/Prenta.*tveimur/d' \
 					-e '/Ferill m.lsins . Al.ingi/d' \
 					-e :a -e '/^\n*$/{$d;N;};/\n$/ba' \
 					-e '/<!-- Virk vefm..\?ling byrjar/,$d' \
@@ -65,13 +61,13 @@ do
 					-e "s/http:\/\/www.althingi.is\/lagas\/$i\///g" \
 					-e 's/\(<\/body>\)\?<\/html>//' \
 					-e 's/\(<head>\)\?<title>/<head><title>/' -e 's/<\/title>\(<\/head>\)\?/<\/title><\/head>/' \
-					$file
-#			tidy -q -m $file
+					| pandoc +RTS -K1844674407370955161 -RTS -f html -t markdown > `dirname $file`/`basename $file .html`.md
+			rm $file
 		done
 
+		ln -s kaflar.md README.md
 		git add .
 		git commit -q -m "Útgáfa $i" --date "`date --date "$COMMITDATE"`"
-		echo "Commit version $i"
 	)
 done
 
